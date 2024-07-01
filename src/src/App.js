64cd0eb8b30e2fcd354d5b2502858f016f37d564
dbann <43852989+dbann@users.noreply.github.com>
2024-07-01 11:36:13 +0100

    Create App.js

---
+++ src/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ScienceFundingCostVisualizer from './ScienceFundingCostVisualizer';
+
+function App() {
+  return (
+    <div className="App">
+      <ScienceFundingCostVisualizer />
+    </div>
+  );
+}
+
+export default App;
