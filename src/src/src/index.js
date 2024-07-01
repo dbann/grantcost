29154f1a7ab76d185fd55849d653091494287dd8
dbann <43852989+dbann@users.noreply.github.com>
2024-07-01 11:42:49 +0100

    Update index.js

--- src/src/src/index.js
+++ src/src/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import ScienceFundingCostVisualizer from './ScienceFundingCostVisualizer';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ScienceFundingCostVisualizer />
   </React.StrictMode>,
   document.getElementById('root')
 );
